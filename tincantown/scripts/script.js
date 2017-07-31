0c951ad0bd998a2db7afe66b83e56bb666349245
lucasljordan <lucasl.jordan@gmail.com>
2017-07-31 22:14:27 +1000

    - added responsive menu

---
+++ tincantown/scripts/script.js
@@ -0,0 +1,17 @@
+$(document).ready( function(){
+
+  console.log("HERE");
+
+  $(".responsive-menu").on("click touchstart", function( event ){
+
+    var $nav = $(this).closest("nav").find("ul");
+    if ( $nav.attr("data-expanded") == "false"  ) {
+      $(this).closest("nav").find("ul").attr("data-expanded", "true")
+
+    } else if ( $nav.attr("data-expanded") == "true" ) {
+      $(this).closest("nav").find("ul").attr("data-expanded", "false")
+
+    }
+  })
+
+});

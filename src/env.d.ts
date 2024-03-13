d370e17e6bf0aebf615cd261b20c35c2db07c9bd
lucas jordan <lucasl.jordan@gmail.com>
2024-03-13 18:32:14 +1100

    bootstrapped application

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,7 @@
+/// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+/** -------------------------------------------------------------------------------------- */
+/** Custom global types:                                                                   */
+/** -------------------------------------------------------------------------------------- */
+type NavLink = { title: string; url: string; active?: boolean; target?: string; };

1f35bdaeb6ba85b0a928fe9d5f6b14c7d5bd3a21
lucas jordan <lucasl.jordan@gmail.com>
2024-04-23 20:58:35 +1000

    updated config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://lucasljordan.github.io',
+  site: 'http://www.lucasjordan.dev',
 });
